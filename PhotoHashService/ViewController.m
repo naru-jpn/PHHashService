cab1f2ea337a7fabe9170740028015ba14f58353
1_am_a_geek <tmy0x3@icloud.com>
2016-02-15 15:02:50 +0900

    検証用UIを追加

--- PhotoHashService/ViewController.m
+++ PhotoHashService/ViewController.m
@@ -12,6 +12,10 @@
 
 @interface ViewController ()
 
+@property (nonatomic) UIButton *runButton;
+@property (nonatomic) UIButton *clearButton;
+@property (nonatomic) UIButton *getButton;
+
 @end
 
 @implementation ViewController
@@ -19,14 +23,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-//    [[PHHashService sharedService] clear];
+    _runButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [_runButton setTitle:@"Run" forState:UIControlStateNormal];
+    [_runButton addTarget:self action:@selector(runService) forControlEvents:UIControlEventTouchUpInside];
+    _clearButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [_clearButton setTitle:@"Clear" forState:UIControlStateNormal];
+    [_clearButton addTarget:self action:@selector(clearService) forControlEvents:UIControlEventTouchUpInside];
+    _getButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [_getButton setTitle:@"Get" forState:UIControlStateNormal];
+    [_getButton addTarget:self action:@selector(getService) forControlEvents:UIControlEventTouchUpInside];
     
-    [[PHHashService sharedService] run];
+    [self.view addSubview:_runButton];
+    [self.view addSubview:_clearButton];
+    [self.view addSubview:_getButton];
        
     NSLog(@"all: %@", [[PHHashService sharedService] allHashedObjects]);
     
 //    NSArray *hashStrings = @[@"5FCC26914F2EB1C7C88614246B45B2CA", @"2E91168C2B18DAE1028BE41F4335ED25", @"E5620A09BC055BA39314D6B9407D4521"];
 //    NSLog(@"excluded local identifires: %@", [[PHHashService sharedService] localIdentifiersForHashStrings:hashStrings]);
+}
+
+- (void)viewWillLayoutSubviews
+{
+    [_runButton sizeToFit];
+    [_clearButton sizeToFit];
+    [_getButton sizeToFit];
+    _runButton.center = CGPointMake(self.view.bounds.size.width/2 - 40, self.view.bounds.size.height/2);
+    _clearButton.center = CGPointMake(self.view.bounds.size.width/2 + 40, self.view.bounds.size.height/2);
+    _getButton.center = CGPointMake(self.view.bounds.size.width/2, self.view.bounds.size.height/2 + 40);
+}
+
+- (void)runService
+{
+    [[PHHashService sharedService] run];
+}
+
+- (void)clearService
+{
+    [[PHHashService sharedService] clear];
+}
+
+- (void)getService
+{
+    NSLog(@"%@", [[PHHashService sharedService] allHashedObjects]);
 }
 
 - (void)didReceiveMemoryWarning {
